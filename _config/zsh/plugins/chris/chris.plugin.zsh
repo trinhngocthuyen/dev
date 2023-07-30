d28311958570b7ce10309f5740db265a4a863cc6
Thuyen Trinh <trinhngocthuyen@gmail.com>
2023-07-30 09:54:12 +0700

    Update chris.plugin.zsh

--- _config/zsh/plugins/chris/chris.plugin.zsh
+++ _config/zsh/plugins/chris/chris.plugin.zsh
@@ -1,10 +1,18 @@
 # ----- ALIASES -----------------------
+
 alias gs="git status"
 alias gpp="git push origin head"
 alias bpod="bundle exec pod"
 alias bpi="bundle exec pod install"
-alias vscode="open -a VSCode"
+alias bfastlane="bundle exec fastlane"
 alias stree="open -a Sourcetree"
+
+if [[ -d "/Applications/Visual Studio Code.app" ]]; then
+    VSCODE_APP_NAME="Visual Studio Code"
+elif [[ -d "/Applications/VSCode.app" ]]; then
+    VSCODE_APP_NAME="VSCode"
+fi
+alias code="open -a \"${VSCODE_APP_NAME}\""
 
 function check_ports() {
 	lsof -i -P -n | grep LISTEN 2> /dev/null
@@ -13,4 +21,21 @@ function check_ports() {
 function sim_record() {
 	rm -rf "$1"
 	xcrun simctl io booted recordVideo "$1"
+}
+
+function git_default_branch() {
+    git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'
+}
+
+function switch_and_fetch() {
+    local default_branch=$(git_default_branch)
+    git checkout -f ${default_branch}
+    git reset --hard && git clean -df
+    git pull origin ${default_branch}
+}
+
+function rebase_with_upstream() {
+    local default_branch=$(git_default_branch)
+    git fetch origin ${default_branch}
+    git rebase FETCH_HEAD
 }
