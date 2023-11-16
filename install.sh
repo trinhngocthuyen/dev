ac410fd1923a9e3c3eb87a782575d10c45cf337b
Thuyen Trinh <trinhngocthuyen@gmail.com>
2023-11-16 20:57:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ rm -rf /tmp/dev
 git clone --single-branch --depth 1 https://github.com/trinhngocthuyen/dev.git /tmp/dev
 cd /tmp/dev
 
-echo "DEPS: $1"
-which python3 && python3 install.py --dep $1
+echo "DEPS: $@"
+which python3 && python3 install.py --dep $@
